ca914a6249b00096945031a128f7c93318da0834
fem97 <fem.nwu@gmail.com>
2022-02-06 09:51:25 +0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const userHandler = require('./routeHandler/userHandler');
 
+// db connection
+mongoose.connect("mongodb://localhost/checkin").then(() => {
+    console.log("db connection established");
+}).catch(err => console.log("not connected"));
+
 const app = express();
+app.use(express.json());
 
 app.use('/user', userHandler);
 
--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,13 +1,22 @@
 const express = require('express');
-
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
+const User = require('../schemas/userSchema');
+
 router.post('/signup', async (req, res) => {
-    res.send("signup route")
+    try {
+        const newUser = new User(req.body);
+        newUser.save();
+        res.send(newUser)
+    } catch (error) {
+        res.send("Not saved. Please try again");
+    }
 })
 
 router.post('/login', async (req, res) => {
-    res.send("login route")
+    console.log(req.body);
+    res.send(req.body)
 })
 
 module.exports = router;
