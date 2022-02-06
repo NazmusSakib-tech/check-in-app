346389462d62a3baaaf23fa91f5c4e727117befd
Shahriar <fem.nwu@gmail.com>
2022-02-06 10:25:51 +0600

    updated

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -7,6 +7,8 @@ const User = require('../schemas/userSchema');
 router.post('/signup', async (req, res) => {
     try {
         const newUser = new User(req.body);
+        const hashedPassword = await bcrypt.hash(req.body.pinCode, 10);
+        newUser.pinCode = hashedPassword;
         newUser.save();
         res.send(newUser)
     } catch (error) {
@@ -15,8 +17,17 @@ router.post('/signup', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    console.log(req.body);
-    res.send(req.body)
+    const user = await User.findOne({ mobile: req.body.mobile });
+    console.log(user);
+    const isValid = await bcrypt.compare(req.body.pinCode, user.pinCode);
+    if (isValid) {
+        // DO What you want to do after successfull login
+
+        res.send(req.body)
+    } else {
+        res.send("Authentication failed");
+    }
+
 })
 
 module.exports = router;
