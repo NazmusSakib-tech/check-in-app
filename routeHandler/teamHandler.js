70bda9c0422a4777cd328107d61ee314ec970499
Shahriar <fem.nwu@gmail.com>
2022-02-06 12:47:49 +0600

    updated

--- routeHandler/teamHandler.js
+++ routeHandler/teamHandler.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Member = require('../schemas/memberSchema');
 
 router.post('/createteam', async (req, res) => {
     const { teamCode, teamName, teamDescription, mobile } = req.body;
-    // const 
-    res.send("create team")
+    // const member = await Member.findOne({ mobile })
+
+    res.send("member")
 })
 
 
---
+++ schemas/teamSchema.js
@@ -0,0 +1,16 @@
+// const mongoose = require('mongoose');
+// const teamSchema = mongoose.Schema({
+//     teamCode: String, // unique
+//     name: String,
+//     desc: String,
+//     parentTeam: String,
+//     teamMembers: [
+//         {
+//             mobile: String,
+//             memberRole: String, // owner, admin, member, pending
+//         }
+//     ]
+
+// })
+
+// // const Team = 
